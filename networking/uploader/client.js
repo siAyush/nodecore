523bae4cbd8e33d0b5d58e85e99f334aaa9a38ac
Ayush <siayush.gupta@gmail.com>
2025-04-06 17:42:28 +0530

    uploader

---
+++ networking/uploader/client.js
@@ -0,0 +1,50 @@
+// Grab a file
+// Send the file to the server
+const net = require("net");
+const fs = require("fs/promises");
+const path = require("path");
+
+let fileReadStream;
+
+const socket = net.createConnection(
+  { host: "localhost", port: 8000 },
+  async () => {
+    const fileName = path.basename(process.argv[2]);
+    socket.write(`fileName: ${fileName}-->`);
+
+    const fileHandle = await fs.open(fileName, "r");
+    fileReadStream = fileHandle.createReadStream();
+    const fileSize = (await fileHandle.stat()).size;
+
+    // For showing upload progress
+    let uploadedPercentage = 0;
+    let bytesUploaded = 0;
+
+    // Reading from source file and sending to server
+    fileReadStream.on("data", (data) => {
+      if (!socket.write(data)) {
+        console.log("stop sending data");
+        fileReadStream.pause();
+      }
+      bytesUploaded += data.length;
+      let newPercentage = Math.floor((bytesUploaded / fileSize) * 100);
+      //console.log(`bytes uploaded: ${bytesUploaded}`)
+
+      if (newPercentage !== uploadedPercentage) {
+        uploadedPercentage = newPercentage;
+        console.log(`Uploading... ${uploadedPercentage}%`);
+      }
+    });
+
+    socket.on("drain", () => {
+      console.log("drained client");
+      fileReadStream.resume();
+    });
+
+    fileReadStream.on("end", async () => {
+      console.log("finished");
+      fileHandle.close();
+      socket.end();
+    });
+  }
+);
---
+++ networking/uploader/server.js
@@ -0,0 +1,51 @@
+// Server will receive the data of the file of the client
+// Treat data and return to the client
+
+const net = require("net");
+const fs = require("fs/promises");
+
+const server = net.createServer(() => {});
+
+server.on("connection", (socket) => {
+  // Socket refers to the client endpoint
+  console.log("New connection!");
+  let fileHandle, fileWriteStream;
+
+  socket.on("data", async (data) => {
+    console.log("buffer received from client:", data);
+    if (!fileHandle) {
+      // Pause receiving data to wait for the file to be opened
+      socket.pause();
+
+      const endFileNameIndex = data.indexOf("-->");
+      const fileName = data.subarray(10, endFileNameIndex).toString("utf-8");
+      console.log(fileName);
+
+      fileHandle = await fs.open(`storage/${fileName}`, "w");
+      fileWriteStream = fileHandle.createWriteStream();
+
+      fileWriteStream.write(data.subarray(endFileNameIndex + 3));
+
+      socket.resume();
+      fileWriteStream.on("drain", () => {
+        socket.resume();
+      });
+    } else {
+      if (!fileWriteStream.write(data)) {
+        console.log("server> stop sending data");
+        socket.pause();
+      }
+    }
+  });
+
+  socket.on("end", () => {
+    fileHandle.close();
+    fileHandle = undefined;
+    fileWriteStream = undefined;
+    console.log("Uploaded ended");
+  });
+});
+
+server.listen(8000, () => {
+  console.log("Uploader server opened on", server.address());
+});
